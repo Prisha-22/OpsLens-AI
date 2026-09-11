8b026a93822474d5452f7f92d136dd8ffe861db0
Prisha Shah <prishashah224@gmail.com>
2026-09-11 20:12:50 +0530

    Support Neon database connection

--- python/database.py
+++ python/database.py
@@ -1,8 +1,15 @@
+import os
 import psycopg2
+
 from config import DB_CONFIG
 
 
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
     return psycopg2.connect(
         host=DB_CONFIG["host"],
         database=DB_CONFIG["database"],
